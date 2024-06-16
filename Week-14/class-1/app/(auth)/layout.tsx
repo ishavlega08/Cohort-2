df2c59c24f20cb3783f2ca56a2a0a509363199d0
Ishav <ishavlega@gmail.com>
2024-06-16 12:02:36 +0530

    class-1 completed

---
+++ Week-14/class-1/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+
+export default function SigninLayout({
+    children
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return <div>
+        <div className="border-b text-center p-1">
+            20% off for the next 3 days
+        </div>
+        {children}
+    </div>
+}
