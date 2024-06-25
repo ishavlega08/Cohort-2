0f7e156a50895b1378774475457a2ab102900045
Ishav <ishavlega@gmail.com>
2024-06-25 23:47:54 +0530

    class-2 mid

---
+++ Week-16/class-1/apps/docs/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Admin } from "@repo/ui/admin";
+
+export default function() {
+    return <div>
+        Hi there from docs app admin page
+        <Admin />
+    </div>
+}
---
+++ Week-16/class-1/apps/react-app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from '@repo/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <Button appName="React">
+        hi there
+      </Button>
+    </div>
+  )
+}
+
+export default App
