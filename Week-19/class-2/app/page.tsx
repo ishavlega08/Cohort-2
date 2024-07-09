2adf12d1dcd5dde2de1ebae393a7725bb9193829
Ishav <ishavlega@gmail.com>
2024-07-09 11:17:25 +0530

    class-2 completed

---
+++ Week-19/class-2/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import revalidate from "./todos/action/actions1";
+
+export default async function Home() {
+  const response = await fetch("https://sum-server.100xdevs.com/todos", {
+    next: {
+      tags: ["todos"]
+    }
+  });
+  const data = await response.json();
+  revalidate()
+
+  return (
+    <div>
+      {data.todos.map((todo: any) => <div key={todo.id}>
+        {todo.title}
+        {todo.description}
+      </div>)}
+    </div>
+  );
+}
